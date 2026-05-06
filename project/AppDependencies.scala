bd7cfa51a2933f76f2a8ec154591da9724193d55
maykenworthy <247179073+maykenworthy@users.noreply.github.com>
2026-05-06 13:28:19 +0100

    CARF-500: Updated various configurations, added scalafmt, added PlaceholderController

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,22 +1,29 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.1.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val bootstrapVersion        = "10.1.0"
+  private val hmrcMongoVersion        = "2.12.0"
+  private val playFrontendHmrcVersion = "12.32.0"
+  private val catsCoreVersion         = "2.13.0"
+  private val commonsValidatorVersion = "1.10.0"
+  private val libphonenumberVersion   = "8.13.47"
 
   val compile = Seq(
+    "commons-validator"             % "commons-validator"          % commonsValidatorVersion,
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.8.0",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion
-
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "12.8.0",
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
+    "com.googlecode.libphonenumber" % "libphonenumber"             % libphonenumberVersion,
+    "org.typelevel"                %% "cats-core"                  % catsCoreVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.17.0"
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
+    "org.scalatestplus"    %% "scalacheck-1-17"         % "3.2.17.0",
+    "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
--- test/controllers/auth/AuthControllerSpec.scala
+++ test/controllers/auth/AuthControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.auth
 
 import base.SpecBase
@@ -38,7 +54,7 @@ class AuthControllerSpec extends SpecBase with MockitoSugar {
         val encodedContinueUrl  = URLEncoder.encode(appConfig.exitSurveyUrl, "UTF-8")
         val expectedRedirectUrl = s"${appConfig.signOutUrl}?continue=$encodedContinueUrl"
 
-        status(result) mustEqual SEE_OTHER
+        status(result)                 mustEqual SEE_OTHER
         redirectLocation(result).value mustEqual expectedRedirectUrl
         verify(mockSessionRepository, times(1)).clear(eqTo(userAnswersId))
       }
@@ -67,7 +83,7 @@ class AuthControllerSpec extends SpecBase with MockitoSugar {
         val encodedContinueUrl  = URLEncoder.encode(routes.SignedOutController.onPageLoad().url, "UTF-8")
         val expectedRedirectUrl = s"${appConfig.signOutUrl}?continue=$encodedContinueUrl"
 
-        status(result) mustEqual SEE_OTHER
+        status(result)                 mustEqual SEE_OTHER
         redirectLocation(result).value mustEqual expectedRedirectUrl
         verify(mockSessionRepository, times(1)).clear(eqTo(userAnswersId))
       }
