308553609a54d14cf74848d0973c86d3d3466794
AishaBi <43678132+AishaBi@users.noreply.github.com>
2026-06-11 14:03:53 +0100

    pr comments

--- app/viewmodels/checkAnswers/combined/OrganisationOrIndividualSummary.scala
+++ app/viewmodels/checkAnswers/combined/OrganisationOrIndividualSummary.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package viewmodels.combined
+package viewmodels.checkAnswers.combined
 
 import controllers.combined.routes
 import models.{ChangeMode, OrganisationOrIndividual, UserAnswers}
