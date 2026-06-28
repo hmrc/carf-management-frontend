765aaa74b2d8da17c69eba49c168e9c8baa1013f
matthew-goh <186605436+matthew-goh@users.noreply.github.com>
2026-06-28 18:58:39 +0100

    Fix flaky tests in FindAddressFormProviderSpec

--- test/forms/FindAddressFormProviderSpec.scala
+++ test/forms/FindAddressFormProviderSpec.scala
@@ -83,7 +83,7 @@ class FindAddressFormProviderSpec extends StringFieldBehaviours {
     val maxLength = 35
 
     "must bind valid data" in {
-      forAll(stringsWithMaxLength(maxLength) -> "validString") { string =>
+      forAll(nonEmptyStringsWithMaxLength(maxLength) -> "validString") { string =>
         val result = form.bind(Map("postcode" -> "SW1A 1AA", fieldName -> string))
         result.errors.filter(_.key == fieldName) mustBe empty
       }
@@ -109,7 +109,7 @@ class FindAddressFormProviderSpec extends StringFieldBehaviours {
     behave like fieldThatBindsValidData(
       form,
       fieldName,
-      stringsWithMaxLength(maxLength)
+      nonEmptyStringsWithMaxLength(maxLength)
     )
 
     behave like fieldWithMaxLength(
